0271aeb79e91a71b2b70c5dd781cfb82e5a471b2
milad <zmilad97@gmail.com>
2020-05-06 19:06:33 +0430

    added config.txt file , Config class , and generateWallet method in WalletService , and connectionTest Controller

--- src/main/java/com/github/zmilad97/restfulblockchainwallet/Controller/Controller.java
+++ src/main/java/com/github/zmilad97/restfulblockchainwallet/Controller/Controller.java
@@ -1,5 +1,6 @@
 package com.github.zmilad97.restfulblockchainwallet.Controller;
 
+import com.github.zmilad97.restfulblockchainwallet.Config.Config;
 import com.github.zmilad97.restfulblockchainwallet.Service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,29 +9,50 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 @RestController
 public class Controller {
     WalletService walletService;
+    Config config;
 
     @Autowired
-    public Controller(WalletService walletService) {
+    public Controller(WalletService walletService,Config config) {
         this.walletService = walletService;
+        this.config = config;
+    }
+
+    @RequestMapping(value = "/config/new", method = RequestMethod.POST)
+    public String newConfig(@RequestBody String address) {
+        return config.setAddress(address);
+    }
+
+    @RequestMapping("/config/current")
+    public String currentConfig() {
+        return config.getAddress();
+
+    }
+
+    @RequestMapping("/connectionTest")
+    public String testConnection(){
+        if (config.connectionTest().equals("200"))
+            return "Wallet is connected to the BlockChain core";
+        else
+            return "Connection problem , wallet is not connected to the BlockChain core !";
     }
 
     @RequestMapping("/add")
-    public void addWallet(){
-
+    public List addWallet() throws NoSuchAlgorithmException {
+        return walletService.generateWallet(config);
     }
 
-    @RequestMapping(value = "/trx",method = RequestMethod.POST)
-    public void newTrx(){
+    @RequestMapping(value = "/trx", method = RequestMethod.POST)
+    public void newTrx() {
 
     }
 
     @RequestMapping(value = "/test")
-    public void test() throws NoSuchAlgorithmException {
-        walletService.generateWallet();
+    public void test() {
     }
 
 
--- src/main/java/com/github/zmilad97/restfulblockchainwallet/Service/WalletService.java
+++ src/main/java/com/github/zmilad97/restfulblockchainwallet/Service/WalletService.java
@@ -1,5 +1,6 @@
 package com.github.zmilad97.restfulblockchainwallet.Service;
 
+import com.github.zmilad97.restfulblockchainwallet.Config.Config;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Service
@@ -17,30 +20,30 @@ public class WalletService {
     Cryptography cryptography = new Cryptography();
     Random random = new Random();
 
-    public void generateWallet() throws NoSuchAlgorithmException {
-
+    public List<String> generateWallet(Config config) throws NoSuchAlgorithmException {
         String priKey = cryptography.toHexString(cryptography.getSha(String.valueOf(random.nextDouble())));
         String pubKey = cryptography.toHexString(cryptography.getSha(String.valueOf(random.nextDouble())));
-
+        List<String> keys = new ArrayList<>();
         System.out.println("priKey" + priKey);
         System.out.println("pubKey" + pubKey);
 
         try {
 
             CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-            HttpPost httpPost = new HttpPost("http://localhost:8181/wallet/add");   //TODO : variable address
-//            Gson gson = new Gson();
-//
+            HttpPost httpPost = new HttpPost(config.getAddress()+"/add");
             StringEntity params = new StringEntity(priKey);
             httpPost.addHeader("Content-Type", "application/json");
             httpPost.setEntity(params);
             CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
             System.out.println(httpResponse.getEntity().getContent());
+            keys.add(priKey);
+            keys.add(pubKey);
+            return keys;
         } catch (IOException e) {
             e.printStackTrace();
             e.printStackTrace();
         }
-
+        return null;
     }
 
 }
